cc900758ecc04976c66d6a2eaa40718b21c61c26
Ron1890 <vero.diss90@gmail.com>
2026-04-19 15:24:49 +0100

    return to pg and style

--- src/index.js
+++ src/index.js
@@ -61,7 +61,8 @@ cityTimeZone=moment.tz.guess();
           ${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small>
         </div>
       </div>
-      <a href="index.html">Back to Homescreen<a>
+      <div class=home-link>
+      <a href= "index.html">Back to Homepage</a> </div>
     `;
   }
 
